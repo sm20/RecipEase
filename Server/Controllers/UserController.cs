fd2d98d37e508b1254c6b37ae9da1b114f6d9283
sm20 <sajidmahmood20@gmail.com>
2021-03-22 19:12:22 -0600

    UserController documentation added

--- Server/Controllers/UserController.cs
+++ Server/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RecipEase.Server.Data;
@@ -10,6 +11,9 @@ using RecipEase.Shared.Models.Api;
 
 namespace RecipEase.Server.Controllers
 {
+    [Authorize]
+    [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
@@ -21,14 +25,34 @@ namespace RecipEase.Server.Controllers
             _context = context;
         }
 
-        // GET: api/User
+        /// <summary>
+        /// Returns all User credential information.
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Retrieves all items and all attributes from the `user`
+        /// table.
+        /// A 'select*' query with a 'where' clause to find the list of usernames
+        ///and their associated attributes.
+        /// </remarks>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ApiUser>>> GetApiUser()
         {
             return await _context.ApiUser.ToListAsync();
         }
 
-        // GET: api/User/5
+        /// <summary>
+        /// Returns the User with the given username id.
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Retrieves the object with the given username id value, in the username column, from the
+        /// `User` table, if it exists.
+        ///
+        /// A 'select*' query with a 'where' clause to find the username id
+        /// and its associated attributes.
+        /// </remarks>
+        /// <param name="id">The Username ID of the User to retrieve.</param>
         [HttpGet("{id}")]
         public async Task<ActionResult<ApiUser>> GetApiUser(string id)
         {
@@ -42,9 +66,21 @@ namespace RecipEase.Server.Controllers
             return apiUser;
         }
 
-        // PUT: api/User/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// Update the information of an existing user
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Updates information of existing user
+        /// if the user exists in the User table of the database.
+        ///
+        /// An Update operation is used to update the User in the database if
+        /// the user exists.
+        /// </remarks>
+        ///<param name="id">The username of the User to update.</param>
+        ///<param name="apiUser">The User object to be updated.</param>
         [HttpPut("{id}")]
+        [Consumes("application/json")]
         public async Task<IActionResult> PutApiUser(string id, ApiUser apiUser)
         {
             if (id != apiUser.Id)
@@ -73,9 +109,20 @@ namespace RecipEase.Server.Controllers
             return NoContent();
         }
 
-        // POST: api/User
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /// <summary>
+        /// Add a new User
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Add a new User to the User relation,
+        /// if the username does not exist in the User relation of the database.
+        ///
+        /// 
+        /// An Insert  operation to insert a new User is performed.
+        /// </remarks>
+        ///<param name="apiUser">The User object to be updated.</param>
         [HttpPost]
+        [Consumes("application/json")]
         public async Task<ActionResult<ApiUser>> PostApiUser(ApiUser apiUser)
         {
             _context.ApiUser.Add(apiUser);
@@ -98,7 +145,18 @@ namespace RecipEase.Server.Controllers
             return CreatedAtAction("GetApiUser", new { id = apiUser.Id }, apiUser);
         }
 
-        // DELETE: api/User/5
+        /// <summary>
+        /// Delete aUser
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Delete a User from the database,
+        /// if the user exists in the User relation of the database.
+        ///
+        /// 
+        /// A Delete operation to delete a User is performed.
+        /// </remarks>
+        ///<param name="id">The username of the User to delete.</param>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteApiUser(string id)
         {
